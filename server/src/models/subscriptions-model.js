a4a0c033538d499990843fb1a6c9a05610e0ddc6
Hassan Nadeem <125460447+Hassannadeem861@users.noreply.github.com>
2024-10-15 11:24:27 +0500

    subscriptionsSchema is almost done

---
+++ server/src/models/subscriptions-model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionsSchema = new Schema(
+  {
+
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+  },
+
+  { timestamps: true }
+  
+);
+
+const subscriptions = mongoose.model("Subscription", subscriptionsSchema);
+export default subscriptions;
